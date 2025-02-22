09c7f916a81460bb2869ab6ff4e672315fca70ac
Harish S <119594385+Harish-egov@users.noreply.github.com>
2025-02-22 23:43:29 +0530

    Update deployer.groovy

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -47,7 +47,10 @@ spec:
                 stage('Deploy Images') {
                         container(name: 'egov-deployer', shell: '/bin/sh') {
                             sh """
-                                apk add --no-cache aws-cli
+                                apk add python curl unzip
+                                curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+                                unzip awscli-bundle.zip
+                                ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
                                 aws sts get-caller-identity
                                 /opt/egov/egov-deployer deploy --helm-dir `pwd`/${pipelineParams.helmDir} -c=${env.CLUSTER_CONFIGS}  -e ${pipelineParams.environment} "${env.IMAGES}"
                             """
