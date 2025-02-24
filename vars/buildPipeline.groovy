281ca567db3c8f1daccbb921da19f8c852fc159f
SriPadma8997-egov <112681948+SriPadma8997-egov@users.noreply.github.com>
2025-02-24 12:10:22 +0530

    Update buildPipeline.groovy

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -16,13 +16,13 @@ spec:
     fsGroup: 1000
   containers:
   - name: kaniko
-    image: gcr.io/kaniko-project/executor:debug-v0.15.0
+    image: gcr.io/kaniko-project/executor:v1.19.0
     imagePullPolicy: IfNotPresent
     securityContext:
       runAsUser: 1000
       allowPrivilegeEscalation: false
     command:
-    - /busybox/cat
+    - echo "kaniko"
     tty: true
     env:
       - name: GIT_ACCESS_TOKEN
