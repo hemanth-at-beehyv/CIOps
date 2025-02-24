e4d523aa3729a3a8ab454a8d00e218139125ebd8
SriPadma8997-egov <112681948+SriPadma8997-egov@users.noreply.github.com>
2025-02-24 11:44:49 +0530

    Update buildPipeline.groovy

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -14,7 +14,7 @@ metadata:
 spec:
   containers:
   - name: kaniko
-    image: gcr.io/kaniko-project/executor:debug-0.24.0
+    image: gcr.io/kaniko-project/executor:debug-v0.24.0
     imagePullPolicy: IfNotPresent
     command:
     - /busybox/cat
