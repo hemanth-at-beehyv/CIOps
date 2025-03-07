0e4febc9bf39cb116af1680182b901e2044e0f00
SriPadma8997-egov <112681948+SriPadma8997-egov@users.noreply.github.com>
2025-03-07 12:15:39 +0530

    Update buildPipeline.groovy

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -21,19 +21,7 @@ spec:
     args:
       - |
         echo "Installing OpenSSL 1.1.1..."
-        apt-get update && apt-get install -y wget build-essential
-        cd /usr/local/src
-        wget https://www.openssl.org/source/openssl-1.1.1w.tar.gz
-        tar -xvzf openssl-1.1.1w.tar.gz
-        cd openssl-1.1.1w
-        ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared zlib
-        make -j\$(nproc) && make install
-        ln -sf /usr/local/ssl/lib/libssl.so.1.1 /usr/lib/libssl.so.1.1
-        ln -sf /usr/local/ssl/lib/libcrypto.so.1.1 /usr/lib/libcrypto.so.1.1
-        echo "OpenSSL 1.1.1 installation completed!"
-    volumeMounts:
-      - name: openssl-cache
-        mountPath: /usr/local/ssl
+        openssl version -a
   containers:
   - name: kaniko
     image: gcr.io/kaniko-project/executor:debug-v1.0.0
@@ -78,8 +66,6 @@ spec:
             name: jenkins-credentials
             key: ciDbpassword                      
     volumeMounts:
-      - name: openssl-cache
-        mountPath: /usr/local/ssl
       - name: jenkins-docker-cfg
         mountPath: /root/.docker
       - name: kaniko-cache
@@ -100,8 +86,6 @@ spec:
     - cat
     tty: true        
   volumes:
-  - name: openssl-cache
-    emptyDir: {}
   - name: kaniko-cache
     persistentVolumeClaim:
       claimName: kaniko-cache-claim
