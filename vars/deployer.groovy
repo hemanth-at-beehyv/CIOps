6e807bbf97ab4b6689d37a6837ff4908ed82a647
Shivam Upadhyay <145538335+shivam-eGov@users.noreply.github.com>
2025-06-16 15:36:31 +0530

    install gke-gcloud-auth-plugin

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -50,6 +50,7 @@ spec:
                                 apk add --no-cache curl jq gnupg
                                 curl -Lo /usr/local/bin/sops https://github.com/getsops/sops/releases/download/v3.7.3/sops-v3.7.3.linux
                                 chmod +x /usr/local/bin/sops
+                                gcloud components install gke-gcloud-auth-plugin
                                 /opt/egov/egov-deployer deploy --helm-dir `pwd`/${pipelineParams.helmDir} -c=${env.CLUSTER_CONFIGS}  -e ${pipelineParams.environment} "${env.IMAGES}"
                             """
                             }
